5e801a95c042433978a4b2b0847046c3a68d59ed
Xerxes Battiwalla <xerxes.battiwalla@bionicvis.com>
2020-07-05 12:44:52 +1000

    Added ability to sweep the array single phosphene at a time

--- server.js
+++ server.js
@@ -3,12 +3,23 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
+const pIds = [10001, 10002, 10003, 10004, 10005, 10006, 10007, 10008];
+
+let newPhosphene = (id, random = false) => {
+  return { id: id, int: random ? Math.round(Math.random() * 10) / 10 : 1 }
+}
+
 let randomPhosphenes = (upTo) => {
-  const pIds = [10001, 10002, 10003, 10004, 10005, 10006, 10007, 10008];
   return pIds
           .sort(() => 0.5 - Math.random())
           .slice(0, upTo)
-          .map(x => { return { id: x, int: Math.round(Math.random() * 10) / 10 } });
+          .map(x => newPhosphene(x, true));
+};
+
+var curr = 0;
+let singlePhospheneSweep = () => {
+  curr += 1;
+  return [newPhosphene(pIds[curr % pIds.length])];
 }
 
 wss.on('connection', (ws) => {
@@ -20,6 +31,7 @@ wss.on('connection', (ws) => {
 
     setInterval(() => {
       let payload = JSON.stringify(randomPhosphenes(4));
+      // let payload = JSON.stringify(singlePhospheneSweep());
       console.log(payload);
       ws.send(payload);
     }, 80);
