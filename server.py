14d85432d92aa51136ad6b1abfb72fb57791da30
Xerxes Battiwalla <xerxes.battiwalla@bionicvis.com>
2020-07-07 12:35:32 +1000

    Fixed bug where inbound messages were not coming.
    It was because the call to time.sleep was blocking and preventing the
      inbound loop from being called

--- server.py
+++ server.py
@@ -4,10 +4,9 @@
 
 import asyncio
 import websockets
-import time
 import random
 
-STIM_INTERVAL = 0.80 #secs between stimulations
+STIM_INTERVAL = 0.20 #secs between stimulations
 
 async def log_message(message):
     print(f"Server received: {message}")
@@ -19,12 +18,14 @@ async def stim_random_phosphene():
     return f'[{{"id":{random.randint(10001, 10008)},"int":1}}]'
 
 async def inbound_message_handler(websocket, path):
+    print("inbound handler")
     async for message in websocket:
         await log_message(message)
 
 async def outbound_payload_handler(websocket, path):
     while True:
-        time.sleep(STIM_INTERVAL) # delay inbetween emitting next event
+        await asyncio.sleep(STIM_INTERVAL)
+        # time.sleep(STIM_INTERVAL) # delay inbetween emitting next event
         #message = await stim_single_phosphene()
         message = await stim_random_phosphene()
         await websocket.send(message)
@@ -32,16 +33,18 @@ async def outbound_payload_handler(websocket, path):
 
 async def run_loop(websocket, path):
     print("Client connected")
-    inbound_task = asyncio.ensure_future(inbound_message_handler(websocket, path)) # There's still a bug that means this doesn't fire. It's an async issue with the way i've done it in Py
     outbound_task = asyncio.ensure_future(outbound_payload_handler(websocket, path))
+    inbound_task = asyncio.ensure_future(inbound_message_handler(websocket, path)) 
     done, pending = await asyncio.wait(
-        [inbound_task, outbound_task],
+        [outbound_task, inbound_task],
         return_when = asyncio.FIRST_COMPLETED
     )
     for task in pending:
-        task.cancel()  # Alternatively, can we just call run_loop again?
+        task.cancel()
     
-    run_loop(websocket, path)
+    print("Client disconnected")
+    print("Re-running loop")
+    # await run_loop(websocket, path)
 
 start_server = websockets.serve(run_loop, port=8080)
 
